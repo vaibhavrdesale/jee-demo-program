c49d7a69051658ea46890896b4a03da0d34fb70c
Desale <vaibhav.desale@capgemini.com>
2023-05-22 12:28:17 +0530

    adding doWhile loop

--- javaDemoOfMethod/src/com/java/primeinteger/PrimeNumber.java
+++ javaDemoOfMethod/src/com/java/primeinteger/PrimeNumber.java
@@ -7,26 +7,29 @@ public class PrimeNumber {
 
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
-
+		int ans;
 		int count;
+		do {
 
-		for (int i = 2; i <= num; i++) {
-			count = 0;
-			for (int j = 1; j <= i; j++) {
+			for (int i = 2; i <= num; i++) {
+				count = 0;
+				for (int j = 1; j <= i; j++) {
 
-				if (i % j == 0) {
+					if (i % j == 0) {
 
-					count++;
+						count++;
+
+					}
 
 				}
-				
+				if (count == 2) {
+					System.out.println(i);
+				}
 			}
-			if(count == 2) {
-				System.out.println(i);
-			}
-		}
-		
-		System.out.println("Thanks for use prime number program");
+
+			System.out.println("retrive press 1");
+			ans = sc.nextInt();
+		} while (ans == 1);
 
 	}
 
